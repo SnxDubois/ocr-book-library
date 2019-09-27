29da319b0610a93b7302750f41c8bd6852f739ac
Sebastien Dubois <snx.dubois@gmail.com>
2019-09-27 12:46:37 +0200

    book library components, services and firebase set up with header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'book-library';
+  constructor() {
+  const firebaseConfig = {
+    apiKey: 'AIzaSyA9Hw2jf2xfcVd3jLyCm8K7Qf9uVtvQ6xI',
+    authDomain: 'book-library-6c547.firebaseapp.com',
+    databaseURL: 'https://book-library-6c547.firebaseio.com',
+    projectId: 'book-library-6c547',
+    storageBucket: '',
+    messagingSenderId: '253610682250',
+    appId: '1:253610682250:web:dacafdda6ba9e70763ca73',
+    measurementId: 'G-4YN4QK9JN9'
+  };
+  firebase.initializeApp(firebaseConfig);
+}
 }
